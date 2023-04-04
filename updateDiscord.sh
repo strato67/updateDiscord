598ce957ec5c77fc24a44518fbfd4d7e0380563b
Saaransh Sharma <saaranshsharma28@gmail.com>
2023-04-03 20:39:39 -0400

    Add files via upload
    initial commit

---
+++ updateDiscord.sh
@@ -0,0 +1,13 @@
+wget https://discordapp.com/api/download?platform=linux -O ~/Downloads/discord.deb
+
+cd ~/Downloads/
+
+sudo dpkg -i discord.deb
+
+rm discord.deb
+
+cd $(dirname $(which discord))
+
+./discord 
+
+exit
